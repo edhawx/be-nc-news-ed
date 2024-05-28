f95f09edcfe45a0813aada3dace01bfbe49e4e11
edhawx <deadlifecorps@gmail.com>
2024-05-28 10:26:34 +0100

    This commit will add .env files, add instructions to readme and also add a checkingdata.js file to check data comes through from index

---
+++ checkingdata.js
@@ -0,0 +1,6 @@
+// const data = require('./db/data/test-data/index');
+
+// console.log(data.userData);
+// console.log(data.commentData);
+// console.log(data.topicData);
+// console.log(data.articleData);
