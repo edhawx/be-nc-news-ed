8ef027f631542a1aa31525b4d19a593df13b5370
edhawx <deadlifecorps@gmail.com>
2024-05-29 12:19:22 +0100

    This commit will: Task 07-> Implement POST to /api/articles/:article_id/comments with a 201 status code, plus implement testing on this endpoint, responding with 400 and 404

--- models/index.js
+++ models/index.js
@@ -1,1 +1,18 @@
-exports.fetchTopics = require('../models/app.models').fetchTopics
+const {
+  fetchTopics,
+  fetchArticleById,
+  fetchArticles,
+  fetchArticleCommentsById,
+  insertCommentToArticle,
+} = require("./app.models");
+
+const { checkUserExists } = require("./users.models");
+
+module.exports = {
+  fetchTopics,
+  fetchArticleById,
+  fetchArticles,
+  fetchArticleCommentsById,
+  insertCommentToArticle,
+  checkUserExists,
+};
