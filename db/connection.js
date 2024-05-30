ae8c19c4630a8e42cae7953a7e0bafdb43573483
edhawx <deadlifecorps@gmail.com>
2024-05-30 12:12:39 +0100

    Will amend comment_count so it has its own separate test to show it is included

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-// require('dotenv').config({
-//   path: `${__dirname}/../.env.${ENV}`,
-// });
+const config = {};
+
+if (ENV === 'production'){
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 const pathToEnvFile = `${__dirname}/../.env.${ENV}`
 
@@ -15,8 +18,8 @@ console.log(`the node environment is... ${ENV}`)
 console.log(`the path is... ${pathToEnvFile}`)
 console.log(`the database is... ${PGDATABASE}`)
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
